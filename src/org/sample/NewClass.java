9ffb08910bb3c21f986ba025184d82c7bc85ee32
Pavithra1702 <pandapavi17@gamil.com>
2022-08-23 10:45:07 +0530

    New class file added

---
+++ src/org/sample/NewClass.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+public class NewClass {
+	
+	public static void main(String[] args) {
+		 System.out.println(7);
+		 System.out.println(9);
+		System.out.println(8);
+		System.out.println(10);
+	}
+
+}
